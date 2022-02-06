9a9fcafcc284717fcc0681b6f3ae944a471ea530
Daniel Wilby <dywilby@gmail.com>
2022-02-05 21:27:45 -0800

    add the canvas and clear it

--- public/index.js
+++ public/index.js
@@ -1,4 +1,21 @@
 "use strict";
+var canvasWidth = 800;
+var canvasHeight = 500;
+var create = function () {
+    var canvas = document.createElement('canvas');
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    canvas.style.display = "block";
+    canvas.style.margin = "auto";
+    document.body.appendChild(canvas);
+    var gl = canvas.getContext("webgl2");
+    if (!gl)
+        throw new Error("Webgl couldn't instanciate");
+    return { canvas: canvas, gl: gl };
+};
 var main = function () {
-    console.log('test');
+    var _a = create(), canvas = _a.canvas, gl = _a.gl;
+    gl.viewport(0, 0, canvasWidth, canvasHeight);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
 
+const canvasWidth = 800;
+const canvasHeight = 500;
+
+const create = () => {
+
+  const canvas = document.createElement('canvas');
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+  canvas.style.display = "block";
+  canvas.style.margin = "auto";
+
+  document.body.appendChild(canvas);
+
+  const gl = canvas.getContext("webgl2");
+
+  if(!gl)
+    throw new Error("Webgl couldn't instanciate");
+
+  return { canvas, gl };
+};
+
 const main = () => {
 
-  console.log('test');
+  const { canvas, gl } = create();
 
+  gl.viewport(0, 0, canvasWidth, canvasHeight);
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 };
